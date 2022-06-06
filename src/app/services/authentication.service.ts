4ab91e5ea1e4f0a04a9e1680d063e686a7ab6dc0
Yogesh <cogdotnet357@iiht.tech>
2022-06-06 11:37:53 +0530

    service files inclded for seller,buyer and register

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
---
+++ src/app/services/register.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string) {
+    return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username, password })
+      .pipe(map(user => {
+        // store user details and jwt token in local storage to keep user logged in between page refreshes
+        localStorage.setItem('user', JSON.stringify(user));
+        this.userSubject.next(user);
+        return user;
+      }));
+  }
+}
