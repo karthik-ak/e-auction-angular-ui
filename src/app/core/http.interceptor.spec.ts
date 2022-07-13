adfe1f12ce1d9e5a30360230205616fcf3d76b26
Yogesh <cogdotnet357@iiht.tech>
2022-07-13 17:33:05 +0530

    issue fix

--- src/app/core/http.interceptor.spec.ts
+++ src/app/core/http.interceptor.spec.ts
@@ -1,16 +1,16 @@
+import { HttpInterceptor } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
-import { HttpInterceptor } from './http.interceptor';
+import { EAuctionHttpInterceptor } from './http.interceptor';
 
 describe('HttpInterceptor', () => {
   beforeEach(() => TestBed.configureTestingModule({
     providers: [
-      HttpInterceptor
+      EAuctionHttpInterceptor
       ]
   }));
 
   it('should be created', () => {
-    const interceptor: HttpInterceptor = TestBed.inject(HttpInterceptor);
+    const interceptor: HttpInterceptor = TestBed.inject(EAuctionHttpInterceptor);
     expect(interceptor).toBeTruthy();
   });
 });
