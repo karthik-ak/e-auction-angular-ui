83641d84baf2ae721fe906865bef20385e08838f
Yogesh <cogdotnet357@iiht.tech>
2022-07-18 21:46:42 +0530

    authentication api url issue fix

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
-const AUTH_API = environment.apiUrl;
+const AUTH_API = `${environment.apiUrl}auth/`;
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
