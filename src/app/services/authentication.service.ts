e4d8a741487502b738e6d8403a705dd70752acbf
Yogesh <cogdotnet357@iiht.tech>
2022-07-18 21:33:01 +0530

    hard code api url removed

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
-const AUTH_API = 'http://localhost:33226/auth/';
+const AUTH_API = environment.apiUrl;
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -13,20 +14,20 @@ const httpOptions = {
 })
 export class AuthenticationService {
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    login(username: string, password: string): Observable<any> {
-      return this.http.post(AUTH_API + 'validateuser/'+ username + '/' + password, {
-        username,
-        password
-      }, httpOptions);
-    }
-  
-    register(username: string, email: string, password: string): Observable<any> {
-      return this.http.post(AUTH_API + 'createuser', {
-        username,
-        email,
-        password
-      }, httpOptions);
-    }
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'validateuser/' + username + '/' + password, {
+      username,
+      password
+    }, httpOptions);
+  }
+
+  register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'createuser', {
+      username,
+      email,
+      password
+    }, httpOptions);
+  }
 }
